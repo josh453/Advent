369b5a21bfa1be524e076ae16b47417dd9d93689
Josh Stewart <joshuajstewart87@gmail.com>
2021-12-28 12:48:29 -0500

    Initial

---
+++ Advent of Code/2021/Day2/Day2.py
@@ -0,0 +1,61 @@
+
+from typing import Tuple
+
+
+sample = [
+    "forward 5",
+    "down 5",
+    "forward 8",
+    "up 3",
+    "down 8",
+    "forward 2",
+    ]
+
+
+
+def find_x_y_position(iterable:list) -> Tuple[int, int]:
+    """
+    Given a list of directions, return the depth and horizontal position
+    """
+    x = int()
+    y = int()
+    for direction in iterable:
+        if "forward" in direction:
+            x += int(direction[-1])
+        if "up" in direction:
+            y -= int(direction[-1])
+        if "down" in direction:
+            y += int(direction[-1])
+    
+    return x,y
+
+def find_x_y_position_with_aim(iterable:list) -> Tuple[int, int]:
+    """
+    Given a list of directions, return the depth and horizontal position using aim
+    """
+    x = int()
+    y = int()
+    aim = int()
+    for direction in iterable:
+        if "forward" in direction:
+            x += int(direction[-1])
+            y += (int(direction[-1]) * aim)
+        if "up" in direction:
+            aim -= int(direction[-1])
+        if "down" in direction:
+            aim += int(direction[-1])
+    
+    return x,y
+
+
+if __name__ == "__main__":
+    with open("Day2/input.txt", "r") as f:
+        my_list = [line.rstrip() for line in f]
+
+        # Part 1
+        x,y = find_x_y_position(my_list)
+        print(f"Part 1: {x*y}")
+
+        # Part 2
+        x,y = find_x_y_position_with_aim(my_list)
+        print(f"Part 2: {x*y}")
